2fb1ad43e4e185a3273d807458804f5e5b4800d4
Eliceo Joel <eliceoherbas@gmail.com>
2024-01-24 23:50:02 -0400

    feature-19: Added fucntionality to add a new day experience for a post

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import BackButton from "@/components/BackButton";
 import Post from "@/components/Post";
-import PostModal from "@/components/modals/PostModal";
-import ConfirmationModal from "@/components/modals/ConfirmationModal";
 
 import { useUserStore } from "@/store/userStore";
-import { getUserPosts, removePost } from "@/services/post";
+import { getUserPosts } from "@/services/post";
 
 import { IPostWithUserName } from "@/interfaces/objects";
 import NoData from "@/components/NoData";
@@ -18,10 +15,9 @@ import NoData from "@/components/NoData";
 function Profile() {
 	const { user } = useUserStore((user) => user);
 	const router = useRouter();
+
 	const [myPosts, setMyPosts] = useState<IPostWithUserName[]>([]);
 	const [postsAreLoading, setPostsAreLoading] = useState(true);
-	const [postToEdit, setPostToEdit] = useState<IPostWithUserName | null>(null);
-	const [postToRemove, setPostToRemove] = useState<IPostWithUserName | null>(null);
 
 	useEffect(() => {
 		async function getMyPostsData(userId: number) {
@@ -38,27 +34,6 @@ function Profile() {
 		}
 	}, []);
 
-	const removePostAction = async () => {
-		if (postToRemove !== null) {
-			// Removes from the UI
-			setMyPosts(myPosts.filter((post) => post.post.id !== postToRemove.post.id));
-			// Removes from database
-			await removePost(postToRemove.post.id);
-			// Close actions dropdown
-			closePostActionsDropdown();
-		}
-	};
-
-	const cancelRemoveAction = () => {
-		closePostActionsDropdown();
-	};
-
-	const closePostActionsDropdown = () => {
-		if (postToRemove !== null) {
-			document.getElementById(`actionsPost${postToRemove.post.id}`)?.removeAttribute("open");
-		}
-	};
-
 	return (
 		<div>
 			<div className="flex justify-center">
@@ -69,7 +44,9 @@ function Profile() {
 			<div className="flex flex-col items-center gap-4">
 				<div className="avatar placeholder">
 					<div className="bg-neutral text-neutral-content rounded-full w-14 h-14">
-						<span className="text-2xl">{user !== null && (user.name.charAt(0).toUpperCase() + user.lastName.charAt(0).toUpperCase())}</span>
+						<span className="text-2xl">
+							{user !== null && user.name.charAt(0).toUpperCase() + user.lastName.charAt(0).toUpperCase()}
+						</span>
 					</div>
 				</div>
 				<h1 className="font-bold text-2xl md:text-3xl">{user?.name + " " + user?.lastName}</h1>
@@ -92,8 +69,9 @@ function Profile() {
 									key={postWithUsername.post.id}
 									postWithUserName={postWithUsername}
 									setPostComments={null}
-									setPostToEdit={setPostToEdit}
-									setPostToRemove={setPostToRemove}
+									setPostToEdit={null}
+									setPostToRemove={null}
+									setPostAction={null}
 								/>
 							))
 						) : (
@@ -102,12 +80,6 @@ function Profile() {
 					</div>
 				)}
 			</div>
-			<PostModal postToEdit={postToEdit} changePostToEdit={setPostToEdit} setMyPosts={setMyPosts} />
-			<ConfirmationModal
-				confirmationText="¿Quieres eliminar este post?"
-				yesAction={removePostAction}
-				cancelAction={cancelRemoveAction}
-			/>
 		</div>
 	);
 }
